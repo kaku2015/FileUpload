cf70f7cfdd09957b491c1e1a18109912937c5bf3
咖枯 <kaku201313@163.com>
2016-12-06 14:33:31 +0800

    退出卸载

--- dataupload/proguard-rules.pro
+++ dataupload/proguard-rules.pro
@@ -1,6 +1,6 @@
 # Add project specific ProGuard rules here.
 # By default, the flags in this file are appended to flags specified
-# in C:\Users\Admin\AppData\Local\Android\sdk/tools/proguard/proguard-android.txt
+# in E:\Users\hyw\AppData\Local\Android\sdk/tools/proguard/proguard-android.txt
 # You can edit the include path and order by changing the proguardFiles
 # directive in build.gradle.
 #
@@ -15,3 +15,6 @@
 #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
 #   public *;
 #}
+
+# retrolambda
+-dontwarn java.lang.invoke.*
---
+++ dataupload/src/main/java/com/skr/fileupload/receiver/SdBroadcastReceiver.java
@@ -0,0 +1,29 @@
+package com.skr.fileupload.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.skr.fileupload.utils.ApkController;
+import com.socks.library.KLog;
+
+/**
+ * @author hyw
+ * @since 2016/12/5
+ */
+public class SdBroadcastReceiver extends BroadcastReceiver {
+    private static final String LOG_TAG = "SdBroadcastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        KLog.i(LOG_TAG, "onReceive");
+
+        String action = intent.getAction();
+        if (action.equals(Intent.ACTION_MEDIA_EJECT)) {
+            KLog.i(LOG_TAG, "sd eject");
+            ApkController.uninstall("com.skr.fileupload", context);
+        } else if (action.equals(Intent.ACTION_MEDIA_MOUNTED)) {
+            KLog.i(LOG_TAG, "sd mounted");
+        }
+    }
+}
