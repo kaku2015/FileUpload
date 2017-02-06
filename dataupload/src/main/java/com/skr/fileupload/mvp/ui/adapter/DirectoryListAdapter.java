971db42d4108944b7eb12fd997c5f9f94d3fdc0d
咖枯 <kaku201313@163.com>
2017-02-06 16:08:39 +0800

    sslsocket

--- dataupload/src/main/java/com/skr/fileupload/mvp/ui/adapter/DirectoryListAdapter.java
+++ dataupload/src/main/java/com/skr/fileupload/mvp/ui/adapter/DirectoryListAdapter.java
@@ -42,14 +42,27 @@ import java.io.RandomAccessFile;
 import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.net.URLEncoder;
+import java.security.KeyStore;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.TrustManagerFactory;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import rx.Observable;
+
+import static com.skr.fileupload.repository.network.ApiConstants.CLIENT_AGREEMENT;
+import static com.skr.fileupload.repository.network.ApiConstants.CLIENT_KET_PASSWORD;
+import static com.skr.fileupload.repository.network.ApiConstants.CLIENT_KEY_KEYSTORE;
+import static com.skr.fileupload.repository.network.ApiConstants.CLIENT_KEY_MANAGER;
+import static com.skr.fileupload.repository.network.ApiConstants.CLIENT_TRUST_KEYSTORE;
+import static com.skr.fileupload.repository.network.ApiConstants.CLIENT_TRUST_MANAGER;
+import static com.skr.fileupload.repository.network.ApiConstants.CLIENT_TRUST_PASSWORD;
 
 /**
  * @author hyw
@@ -165,21 +178,24 @@ public class DirectoryListAdapter extends BaseRecyclerViewAdapter<DirectoryFile>
         fileHolder.mFileNameTv.setText(item.getName());
 
         fileHolder.mUploadBtn.setOnClickListener(view -> {
-            // wifi可用
-            if (NetworkUtils.isWifiAvailable(mContext)) {
-                KLog.i(LOG_TAG, "wifi can use");
-                // wifi已打开
-                if (NetworkUtils.getWifiEnabled(mContext)) {
-                    KLog.i(LOG_TAG, "wifi is Opened");
-                    startUpload(fileHolder, position, item);
-                } else {
-                    KLog.i(LOG_TAG, "wifi is unOpened");
-                    openWifi();
-                }
-            } else {
-                KLog.i(LOG_TAG, "wifi can not use");
-                openWifi();
-            }
+            startUpload(fileHolder, position, item);
+
+
+//            // wifi可用
+//            if (NetworkUtils.isWifiAvailable(mContext)) {
+//                KLog.i(LOG_TAG, "wifi can use");
+//                // wifi已打开
+//                if (NetworkUtils.getWifiEnabled(mContext)) {
+//                    KLog.i(LOG_TAG, "wifi is Opened");
+//                    startUpload(fileHolder, position, item);
+//                } else {
+//                    KLog.i(LOG_TAG, "wifi is unOpened");
+//                    openWifi();
+//                }
+//            } else {
+//                KLog.i(LOG_TAG, "wifi can not use");
+//                openWifi();
+//            }
         });
 
         fileHolder.mPauseBtn.setOnClickListener(view -> mPaths.put(position, true));
@@ -249,14 +265,14 @@ public class DirectoryListAdapter extends BaseRecyclerViewAdapter<DirectoryFile>
 
     private void uploadFile(final File sourceFile, final int p, final Handler handler) {
         new Thread(() -> {
-            Socket socket = null;
             OutputStream outStream = null;
             PushbackInputStream inStream = null;
             RandomAccessFile fileOutStream = null;
+            Socket socket = init();
             try {
                 File encryptedFile = encryptFile(sourceFile);
 
-                socket = new Socket(ApiConstants.HOST, ApiConstants.PORT);
+//                socket = new Socket(ApiConstants.HOST, ApiConstants.PORT);
                 outStream = socket.getOutputStream();
 
 //                String sourceId = GreenDaoManager.getInstance().getSourceIdByPath(encryptedFile.getAbsolutePath());
@@ -335,6 +351,39 @@ public class DirectoryListAdapter extends BaseRecyclerViewAdapter<DirectoryFile>
                 }
             }
         }).start();
+    }
+
+    private SSLSocket init() {
+        SSLSocket socket = null;
+        try {
+            //取得SSL的SSLContext实例
+            SSLContext sslContext = SSLContext.getInstance(CLIENT_AGREEMENT);
+            //取得KeyManagerFactory和TrustManagerFactory的X509密钥管理器实例
+            KeyManagerFactory keyManager = KeyManagerFactory.getInstance(CLIENT_KEY_MANAGER);
+            TrustManagerFactory trustManager = TrustManagerFactory.getInstance(CLIENT_TRUST_MANAGER);
+            //取得BKS密库实例
+            KeyStore kks = KeyStore.getInstance(CLIENT_KEY_KEYSTORE);
+            KeyStore tks = KeyStore.getInstance(CLIENT_TRUST_KEYSTORE);
+            //加客户端载证书和私钥,通过读取资源文件的方式读取密钥和信任证书
+            kks.load(App.getAppContext()
+                    .getResources()
+                    .openRawResource(R.raw.kclient), CLIENT_KET_PASSWORD.toCharArray());
+            tks.load(App.getAppContext()
+                    .getResources()
+                    .openRawResource(R.raw.tclient), CLIENT_TRUST_PASSWORD.toCharArray());
+            //初始化密钥管理器
+            keyManager.init(kks, CLIENT_KET_PASSWORD.toCharArray());
+            trustManager.init(tks);
+            //初始化SSLContext
+            sslContext.init(keyManager.getKeyManagers(), trustManager.getTrustManagers(), null);
+            KLog.i(LOG_TAG, "start ssl connecting");
+            //生成SSLSocket
+            socket = (SSLSocket) sslContext.getSocketFactory().createSocket(ApiConstants.HOST, ApiConstants.PORT);
+            KLog.i(LOG_TAG, "ssl connected");
+        } catch (Exception e) {
+            KLog.e(LOG_TAG, e.toString());
+        }
+        return socket;
     }
 
     @NonNull
--- dataupload/src/main/java/com/skr/fileupload/repository/network/ApiConstants.java
+++ dataupload/src/main/java/com/skr/fileupload/repository/network/ApiConstants.java
@@ -5,9 +5,18 @@ package com.skr.fileupload.repository.network;
  * @since 2016/11/22
  */
 public class ApiConstants {
-//    public static final String HOST = "192.168.0.118";
-//    public static final String HOST = "192.168.0.132";
-    public static final String HOST = "115.29.49.67";
+    public static final String HOST = "192.168.1.115";
+    //    public static final String HOST = "192.168.0.132";
+//    public static final String HOST = "115.29.49.67";
 //    public static final String HOST = "192.168.0.106";
-    public static final int PORT = 7878;
+    public static final int PORT = 7884;
+
+    public static final String CLIENT_KET_PASSWORD = "123456";//私钥密码
+    public static final String CLIENT_TRUST_PASSWORD = "123456";//信任证书密码
+    public static final String CLIENT_AGREEMENT = "TLS";//使用协议
+    public static final String CLIENT_KEY_MANAGER = "X509";//密钥管理器
+    public static final String CLIENT_TRUST_MANAGER = "X509";//
+    public static final String CLIENT_KEY_KEYSTORE = "BKS";//密库，这里用的是BouncyCastle密库
+    public static final String CLIENT_TRUST_KEYSTORE = "BKS";//
 }
+
